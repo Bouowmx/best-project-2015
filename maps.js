5a6e97509811abb34834331ef36a1a7e8c4597af
RzhanRichardZhan <zhan97richard@gmail.com>
2015-01-01 23:10:02 -0500

    Fixed coordinates in moving within the circle and added street indication

--- maps.js
+++ maps.js
@@ -7,29 +7,58 @@ function circleDrawer(m,c,r){
             map: m,
             center: c,
             radius: r,
-	    clickable: false
+	    clickable: true
 	   };
 }
 function initialize() {
     var map = new google.maps.Map(document.getElementById('map-canvas'), {center: {lat: 40.7819, lng: -73.8883}, zoom: 13});
     var marker_0 = new google.maps.Marker({position: new google.maps.LatLng(40.7819, -73.8883), map: map, title: "Player"});
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setOptions({preserveViewport:true,
+				 clickable: false,
+				 markerOptions: {visible: false}
+				 });
     var players = [];
     players[0] = {marker: marker_0,
-                  circle: 0
+                  circle: 0,
+		  path: {
+		      origin : 0,
+		      destination : 0,
+		      travelMode: google.maps.TravelMode.WALKING
+		  } 
 		 };
-    players[0].circle = new google.maps.Circle(circleDrawer(map,players[0].marker.position,4000))
- 
+    players[0].circle = new google.maps.Circle(circleDrawer(map,players[0].marker.position,4000));
+    
+
+    //NOT IN USE, WAS USEFUL; NO LONGER USEFUL
+    //Keeping it for a while though. Might be useful
     var getDistance = function(x,y){
-	console.log(Math.sqrt(Math.pow(x.lat() - y.lat(),2) + Math.pow(x.lng() - y.lng(),2)));
+	//console.log(Math.sqrt(Math.pow(x.lat() - y.lat(),2) + Math.pow(x.lng() - y.lng(),2)));
 	return Math.sqrt(Math.pow(x.lat() - y.lat(),2) + Math.pow(x.lng() - y.lng(),2));
     }
-    google.maps.event.addListener(map, "rightclick", function(e) {
-	if (getDistance(e.latLng,players[0].marker.position) < 0.04620858163331883){
+
+    
+    google.maps.event.addListener(players[0].circle, "mousemove", function(e){
+	
+	    players[0].path.origin = players[0].marker.position;
+	    players[0].path.destination = e.latLng;
+	    directionsService.route(players[0].path,function(result, status){
+		if (status == google.maps.DirectionsStatus.OK){
+		    directionsDisplay.setDirections(result);
+		    directionsDisplay.setMap(map);
+		}
+		
+	    });
+	
+    });
+    google.maps.event.addListener(players[0].circle, "rightclick", function(e) {
+	
 	    players[0].marker.position = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
 	    players[0].circle.setCenter(new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()));
 	    players[0].marker.setMap(map); //setMap will render the marker.
 	    players[0].circle.setMap(map);
-	}
+	
     });
     
 }
