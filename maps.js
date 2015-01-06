31c7666e104f7e6cf79340a3e3f94fffab95e31d
StevenZabolotny <stevezabolotny@gmail.com>
2015-01-06 13:31:30 -0500

    Movement now measures off of distance of the path (the circle is now temporarily obsolete). Also made an end turn button that refreshes the distance.

--- maps.js
+++ maps.js
@@ -38,13 +38,15 @@ function initialize() {
 	return Math.sqrt(Math.pow(x.lat() - y.lat(),2) + Math.pow(x.lng() - y.lng(),2));
     }
 
-    
+
+    var curdist;
+    var remdist = 2000; // This is how much the player has left to move in the current turn.
     google.maps.event.addListener(players[0].circle, "mousemove", function(e){
-	
-	    players[0].path.origin = players[0].marker.position;
-	    players[0].path.destination = e.latLng;
+	players[0].path.origin = players[0].marker.position;
+	players[0].path.destination = e.latLng;
 	    directionsService.route(players[0].path,function(result, status){
 		if (status == google.maps.DirectionsStatus.OK){
+		    curdist = result.routes[0].legs[0].distance.value;
 		    directionsDisplay.setDirections(result);
 		    directionsDisplay.setMap(map);
 		}
@@ -53,13 +55,19 @@ function initialize() {
 	
     });
     google.maps.event.addListener(players[0].circle, "rightclick", function(e) {
-	
+	if (curdist <= remdist) {
+	    remdist = remdist - curdist;
+	    document.getElementById("remdist").innerHTML = "Remaining Distance: " + remdist; // Update the remaining distance.
 	    players[0].marker.position = new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
 	    players[0].circle.setCenter(new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()));
 	    players[0].marker.setMap(map); //setMap will render the marker.
 	    players[0].circle.setMap(map);
-	
+	}
     });
     
+    document.getElementsByName("turn")[0].addEventListener("click", function() {
+	remdist = 2000;
+	document.getElementById("remdist").innerHTML = "Remaining Distance: " + remdist; // Update the remaining distance.
+    });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
