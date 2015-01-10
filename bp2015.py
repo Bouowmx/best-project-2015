8a5517f98b647c2aee2603e06a041b1cc5d8a38e
Bouowmx <bouowmx@gmail.com>
2015-01-09 22:21:26 -0500

    Add uwsgi concurrency and proper websocket closing

--- bp2015.py
+++ bp2015.py
@@ -1,16 +1,26 @@
 import uwsgi
 
+names = []
 def application(env, start_response):
 	uwsgi.websocket_handshake(env['HTTP_SEC_WEBSOCKET_KEY'], env.get('HTTP_ORIGIN', ''))
-	names = []
+	#names = []
 	while (True):
-		message = uwsgi.websocket_recv()
-		if (message[:5] == "name;"):
-			print message
-		if (message[5:] in names):
-			uwsgi.websocket_send("false")
-		else:
-			print message[5:]
-			names.append(message[5:])
-			print names
-			uwsgi.websocket_send("true")
+		msg = uwsgi.websocket_recv()
+		semicolon = msg.find(";")
+		msg_type = msg[:semicolon]
+		msg_data = msg[semicolon + 1:]
+		print '''Message: "''' + msg + '''"''' + "; " + '''Message Type: "''' + msg_type + '''"''' + "; " + '''Message Data: "''' + msg_data + '''"'''
+		if (msg_type == "close"):
+			if (msg_data in names):
+				names.remove(msg_data)
+				print msg_data + " disconnected."
+				print names
+			return [""]
+		if (msg_type == "name"):
+			if (msg_data in names):
+				uwsgi.websocket_send("false")
+			else:
+				names.append(msg_data)
+				print msg_data + " connected."
+				print names
+				uwsgi.websocket_send("true")
