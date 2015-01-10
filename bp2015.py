1028b173c61dd70465e5433506d15b22e66c3ae3
Bouowmx <bouowmx@gmail.com>
2015-01-10 14:52:37 -0500

    Add uwsgi cache: shared memory

--- bp2015.py
+++ bp2015.py
@@ -1,26 +1,28 @@
 import uwsgi
 
-names = []
 def application(env, start_response):
 	uwsgi.websocket_handshake(env['HTTP_SEC_WEBSOCKET_KEY'], env.get('HTTP_ORIGIN', ''))
-	#names = []
+	if (not uwsgi.cache_exists("names")):
+		uwsgi.cache_update("names", "\x1f")
 	while (True):
 		msg = uwsgi.websocket_recv()
-		semicolon = msg.find(";")
-		msg_type = msg[:semicolon]
-		msg_data = msg[semicolon + 1:]
+		msg_type = msg.split("\x1f")[0]
+		msg_data = msg.split("\x1f")[1]
 		print '''Message: "''' + msg + '''"''' + "; " + '''Message Type: "''' + msg_type + '''"''' + "; " + '''Message Data: "''' + msg_data + '''"'''
 		if (msg_type == "close"):
-			if (msg_data in names):
-				names.remove(msg_data)
-				print msg_data + " disconnected."
-				print names
+			names = uwsgi.cache_get("names").split("\x1f")
+			names.remove(msg_data)
+			uwsgi.cache_update("names", "\x1f".join(names))
+			print msg_data + " disconnected."
+			print names
 			return [""]
 		if (msg_type == "name"):
-			if (msg_data in names):
+			if (msg_data in uwsgi.cache_get("names").split("\x1f")):
 				uwsgi.websocket_send("false")
 			else:
+				names = uwsgi.cache_get("names").split("\x1f")
 				names.append(msg_data)
+				uwsgi.cache_update("names", "\x1f".join(names))
 				print msg_data + " connected."
-				print names
+				print uwsgi.cache_get("names").split("\x1f")
 				uwsgi.websocket_send("true")
