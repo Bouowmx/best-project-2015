3fa2bee5b8ba42b464d531ac3a75fc2eff7aae37
Bouowmx <bouowmx@gmail.com>
2015-01-10 22:28:21 -0500

    Add example lobby

--- bp2015.js
+++ bp2015.js
@@ -1,20 +1,47 @@
 (function() {var elements = [];
 var elementEventListeners = [];
 var name = "";
+var roomsGetInterval = 0;
 var state = "";
 var websocket = new WebSocket("ws://bp2015.themafia.info:9090");
 window.onbeforeunload = function(event) {websocket.send("close\x1c" + name);};
 websocket.onclose = function(event) {console.log("WebSocket connection closed: " + event.code);};
 websocket.onerror = function(event) {console.log("WebSocket error occurred.");};
 websocket.onmessage = function(event) {
-	console.log("received: " + event.data);
+	console.log("received: \"" + event.data + "\"");
 	if (state == "login") {
 		name = event.data;
 		if (name == "true") {
 			name = elements[2].value;
-			room();
-		} else {
-			
+			rooms();
+		} else {alert("Name already in use.");}
+	}
+	if (state == "rooms") {
+		var rooms_ = event.data;
+		rooms_ = rooms_.split("\x1d");
+		for (var i = 0; i < rooms_.length; i++) {
+			if (rooms_[i] != "") {
+				rooms_ = rooms_.slice(i);
+				break;
+			}
+			if (i == rooms_.length) {rooms_ = [];}
+		}
+		for (var i = 0; i < rooms_.length; i++) {
+			console.log(rooms_[i]);
+			var index = 7 + 4 * i;
+			if (elements[index]) {elements[7 + 4 * i + 2].replaceChild(document.createTextNode((rooms_[i].split("\x1e").length - 1) + "/4"), elements[index + 2].firstChild);}
+			else {
+				createElement(index, "tr");
+				createElementAppendTextNode(index + 1, "td", i);
+				appendChild(index, index + 1);
+				createElementAppendTextNode(index + 2, "td", (rooms_[i].split("\x1e").length - 1) + "/4");
+				appendChild(index, index + 2);
+				createElementAddEventListener(index + 3, "input", "click", roomJoin);
+				elements[index + 3].setAttribute("type", "button");
+				elements[index + 3].value = "Join";
+				appendChild(index, index + 3);
+				appendChild(2, index);
+			}
 		}
 	}
 };
@@ -55,22 +82,39 @@ function login() {
 	elements[2].focus()
 }
 
-function room() {
-	state = "room";
+function roomCreate(event) {
+	
+}
+
+function roomJoin(event) {
+	console.log("Join room " + (elements.indexOf(event.currentTarget) - 7) / 4);
+}
+
+function rooms() {
+	state = "rooms";
 	stateChange();
 	createElementAppendTextNode(0, "div", "Welcome " + name);
-	createElement(1, "table");
-	createElement(2, "tr");
-	appendChild(1, 2); //Append tr to table.
-	createElementAppendTextNode(3, "th", "ID");
-	appendChild(2, 3); //And append th or td to tr.
-	createElementAppendTextNode(4, "th", "Players");
-	appendChild(2, 4);
+	createElementAddEventListener(1, "input", "click", roomCreate);
+	elements[1].setAttribute("type", "button");
+	elements[1].value = "Create Room";
+	createElement(2, "table");
+	createElement(3, "tr");
+	createElementAppendTextNode(4, "th", "#");
+	appendChild(3, 4);
+	createElementAppendTextNode(5, "th", "Players");
+	appendChild(3, 5);
+	createElementAppendTextNode(6, "th", "Join");
+	appendChild(3, 6);
+	appendChild(2, 3);
 	
 	//Only append the table to the document. Do not append tr or td.
 	document.body.appendChild(elements[0]);
 	document.body.appendChild(elements[1]);
+	document.body.appendChild(elements[2]);
+	roomsGet();
 }
+
+function roomsGet() {websocket.send("rooms\x1c");}
 
 function stateChange() {
 	elementsRemoveEventListeners();
--- bp2015.py
+++ bp2015.py
@@ -4,17 +4,20 @@ def application(env, start_response):
 	uwsgi.websocket_handshake(env['HTTP_SEC_WEBSOCKET_KEY'], env.get('HTTP_ORIGIN', ''))
 	if (not uwsgi.cache_exists("names")):
 		uwsgi.cache_update("names", "\x1f")
+	if (not uwsgi.cache_exists("rooms")):
+		uwsgi.cache_update("rooms", "\x1d")
 	while (True):
 		msg = uwsgi.websocket_recv()
 		msg_type = msg.split("\x1c")[0]
 		msg_data = msg.split("\x1c")[1]
 		print '''Message: "''' + msg + '''"''' + "; " + '''Message Type: "''' + msg_type + '''"''' + "; " + '''Message Data: "''' + msg_data + '''"'''
 		if (msg_type == "close"):
-			names = uwsgi.cache_get("names").split("\x1f")
-			names.remove(msg_data)
-			uwsgi.cache_update("names", "\x1f".join(names))
-			print msg_data + " disconnected."
-			print names
+			if (msg_data != ""):
+				names = uwsgi.cache_get("names").split("\x1f")
+				names.remove(msg_data)
+				uwsgi.cache_update("names", "\x1f".join(names))
+				print msg_data + " disconnected."
+				print names
 			return [""]
 		if (msg_type == "name"):
 			if (msg_data in uwsgi.cache_get("names").split("\x1f")):
@@ -26,3 +29,6 @@ def application(env, start_response):
 				print msg_data + " connected."
 				print uwsgi.cache_get("names").split("\x1f")
 				uwsgi.websocket_send("true")
+		if (msg_type == "rooms"):
+			#uwsgi.websocket_send(uwsgi.cache_get("rooms"))
+			uwsgi.websocket_send("\x1d\x1d1\x1e1\x1f1\x1e2\x1f2\x1e3\x1f3\x1e4\x1f4\x1d3\x1e1\x1f1\x1e2\x1f2\x1e3\x1f3\x1e4\x1f4")
