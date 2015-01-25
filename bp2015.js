53470b1f878e4aef3b31df3526afb10050dcc8ee
Rzhanrichardzhan <zhan97richard@gmail.com>
2015-01-25 15:28:38 -0500

    Some CSS, needs testing

--- bp2015.js
+++ bp2015.js
@@ -346,39 +346,48 @@ function rooms() {
     state = "rooms";
     stateChange();
     roomNumber = -1;
-    createElementAddEventListener(0, "input", "click", function(e) {
+    createElement(0, "div");
+    elementSetAttributes(0,[["className","pure-g"]]);
+    elementSetStyle(0,[["height","100%"]]);
+    createElement(1, "div");
+    elementSetAttributes(1,[["className","pure-u-4-5"]]);
+    elementSetStyle(1,[["height","100%"]]);
+
+
+    createElementAppendTextNode(2, "div", "Welcome " + name);
+    chat(3);
+    createElement(9, "br");
+    createElementAddEventListener(10, "input", "click", function(e) {
 	location.reload();
 	login();
     });
-    elementSetAttributes(0, [["type", "button"], ["value", "Exit"]]);
-    createElement(1, "br");
-    createElementAppendTextNode(2, "div", "Welcome " + name);
-    chat(3);
-    createElementAddEventListener(9, "input", "click", function(e) {
+
+
+
+    elementSetAttributes(10, [["type", "button"], ["id","Exit"],["value", "Exit"]]);
+    createElement(11, "div");
+    elementSetAttributes(11,[["className","pure-u-1-5"]]);
+    elementSetStyle(11,[["height","100%"]]);
+
+    createElementAddEventListener(12, "input", "click", function(e) {
 	state = "waitRoomNumber";
 	clearInterval(intervalRoomsGet);
 	stateChange();
 	websocket.send("roomCreate" + name);
     });
-    elementSetAttributes(9, [["type", "button"], ["value", "Create Room"]]);
-    createElement(10, "table");
-    createElement(11, "tr");
-    createElementAppendTextNode(12, "th", "#");
-    createElementAppendTextNode(13, "th", "Players");
-    createElementAppendTextNode(14, "th", "Join");
-    elementAppendChildren(11, [12, 13, 14]);
-    elementAppendChildren(10, [11]);
+    elementSetAttributes(12, [["type", "button"],["id","Create"], ["value", "Create Room"]]);
+    createElement(13, "table");
+    createElement(14, "tr");
+    createElementAppendTextNode(15, "th", "#");
+    createElementAppendTextNode(16, "th", "Players");
+    createElementAppendTextNode(17, "th", "Join");
+    elementAppendChildren(14, [15, 16, 17]);
+    elementAppendChildren(13, [14]);
     //---css---
-    createElement(15, "div");
-    elementSetAttributes(15,[["className","pure-g"]]);
-    createElement(16, "div");
-    elementSetAttributes(16,[["className","pure-u-4-5"]]);
-    createElement(17, "div");
-    elementSetAtrributes(17,[["className","pure-u-1-5"]]);
-    elementAppendChildren(16,[3,4,5,6,7,8]);
-    elementAppendChildren(17,[9,10]);
-    elementAppendChildren(15,[16,17]);
-    documentBodyAppendElements([0, 1, 2, 15]);
+    elementAppendChildren(1,[2,3,4,5,6,7,8,9,10]);
+    elementAppendChildren(11,[12,13]);
+    elementAppendChildren(0,[1,11]);
+    documentBodyAppendElements([0]);
     websocket.send("rooms");
     intervalRoomsGet = setInterval(function() {websocket.send("rooms")}, 1000);
 }
